deefd555cc2061a1c442dc173955df334198b9b0
배상규 <94222693+sangkyu-bae@users.noreply.github.com>
2026-01-14 23:53:39 +0900

    벡터툴 추가

--- app/service/tool/vector_db_tool.py
+++ app/service/tool/vector_db_tool.py
@@ -1,3 +1,5 @@
+from langchain_core.documents import Document
+from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 
 from app.domain.document.question.document_compressor_service import DocumentCompressorService
@@ -10,6 +12,17 @@ from typing import List
 
 from app.infrastructure.vector_store.vector_filter import VectorFilter
 
+@tool
+def vector_retriever_search(query:str) ->List[Document]:
+    """
+        - vector db에 저장된 문서를 리트리버
+        - 회사 내부 문서에서 찾을 수 있는 정보
+        - 규정, 정책, 내부 서비스, 내부 앱, 지침서
+        - 특정 서비스명, 프로젝트명, 약어, 로컬 규정 등
+        - 내부에서만 존재하는 내용
+    """
+    compress_doc = VectorDBTool(collection="test")
+    return compress_doc.execute(query)
 
 class VectorDBTool(ToolExecutor):
     def __init__(self,collection:str):
